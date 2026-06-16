2502e189403847bc2d7f36da957dafe1dadd6399
Khuleg <26bs38301nest.edu.mn@G301-CC0210.local>
2026-06-16 12:26:17 +0800

    first commit

---
+++ app/search/components/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div
+      className="border-b border-[#E4E4E7] py-4 px-12
+"
+    >
+      <span>AI Tools</span>
+    </div>
+  );
+};
---
+++ app/search/page.tsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <div>
+          <div>
+            <span className="">Image analysis</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
